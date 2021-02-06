f03d619d28f05c6e3d93a7b010b9946a0231f80b
Rktak <2405rounaktak@gmail.com>
2021-02-06 16:00:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Board from "./components/Board";
+import './style/root.scss';
+const App= () =>{
+  return(
+    <div className="app">
+    <h1>TIC TAC TOE</h1>
+    <Board></Board>
+    </div>
+  );
+}
+
+export default App
